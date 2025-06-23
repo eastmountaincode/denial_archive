2066419cabe6a90c237926546554f29282407580
Andrew Boylan <andreweboylan@gmail.com>
2025-06-23 15:18:16 -0700

    fix eslink error

--- src/components/YouPageContent.tsx
+++ src/components/YouPageContent.tsx
@@ -15,7 +15,7 @@ export default function YouPageContent() {
       {/* Right aligned text with right padding */}
       <div className="pr-4 md:pr-8 text-right">
         <h2 className="text-3xl md:text-6xl font-light">
-          I'll be in <Link href="https://www.lailasmith.com/" className="underline decoration-1 underline-offset-4 hover:decoration-4 transition-all">
+          I&apos;ll be in <Link href="https://www.lailasmith.com/" className="underline decoration-1 underline-offset-4 hover:decoration-4 transition-all">
             touch
           </Link>
         </h2>
